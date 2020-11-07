7762309b87da27b5b27aa94c42e1dc70eeb2f167
KimGwangYong <brightdragom@gmail.com>
2020-11-07 14:58:20 +0900

    python_arithmetic operator

---
+++ py-sources/03-01-arrithop-산술.py
@@ -0,0 +1,15 @@
+#arithmetic operator
+
+a = 5
+b = 3
+x = a + b
+
+print('a + b = ', a + b)
+print(a + b)
+print(x)
+print('a - b = ', a - b)
+print('a * b = ', a * b)
+print('a / b = ', a / b) #그냥 결과
+print('a ** b = ', a ** b) #제곱
+print('a // b = ', a // b) #몫
+print('a % b = ', a % b) #나머지
