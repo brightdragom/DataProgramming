6d6d9aec47f5daccfb657716995062e8a9e4ce23
KimGwangYong <brightdragom@gmail.com>
2020-11-07 15:04:37 +0900

    python_relational operator

---
+++ py-sources/03-03-relop-관계.py
@@ -0,0 +1,33 @@
+#relational operator
+
+x = 3
+y = 5
+
+print(x == y)
+
+print(x != y)
+
+print(x > y)
+
+print(x >= y)
+
+print(x < y)
+
+print(x <= y)
+
+print("\n")
+
+#문자열 비교
+
+print('===문자열 비교==')
+print('A' > 'a')    #F
+print('A' < 'a')    #T
+print('A' > 'B')    #F
+print('A' < 'B')    #T
+print('c' < 'f')    #T
+print("\n")
+
+print('Korea' == 'korea')   #F
+print('Korea' != 'korea')   #T
+print('Korea' > 'korea')    #F
+print('Korea' < 'korea')    #T
