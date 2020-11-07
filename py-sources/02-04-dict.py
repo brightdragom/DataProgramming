26f57c8fd04b980dd9448dc32f44fdea27165da8
KimGwangYong <brightdragom@gmail.com>
2020-11-07 14:10:16 +0900

    python_dictionary

---
+++ py-sources/02-04-dict.py
@@ -0,0 +1,12 @@
+#dict { } type : 중괄호 - dictionary
+#Key : Value
+
+dt = {'교수':'반기철','지역':'전주','기간':5}
+dt1 = {'학교':'한국대',"교육내용" : "DSACM1", "종목":"python"}
+dt2 = {'grade':4, '결과' : 'Good' , "Phone" : "010-****-****"}
+
+print("\n")
+print(dt)
+print(dt1)
+print(dt2)
+print("\n")
