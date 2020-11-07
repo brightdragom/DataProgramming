a5d9719d7f3848a633de0694d1f4c08421024564
KimGwangYong <brightdragom@gmail.com>
2020-11-07 15:10:22 +0900

    python_logical operator

---
+++ py-sources/03-03-logiop-논리.py
@@ -0,0 +1,17 @@
+# logical operator
+
+x = 3
+y = 5
+
+print(x == 3)
+print(y > 3)
+print(x ==3 and y > 3)
+print(x != 3 and y > 3)
+print(not x == 3) #x == 3의 반대
+print(not y != 5)
+print("\n")
+
+x = 10
+y = 100
+a = 30
+b = 20
