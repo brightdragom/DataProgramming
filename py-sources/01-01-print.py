2f063cba331b2830c5b9f641e520e9992ca8da1e
KimGwangYong <brightdragom@gmail.com>
2020-11-07 13:30:26 +0900

    python_print function2

--- py-sources/01-01-print.py
+++ py-sources/01-01-print.py
@@ -14,3 +14,35 @@ print("=================")
 
 print('2. 문자열 연습입니다.')   # ''
 print("=================")
+
+print('''3. 문자열
+연습
+입니다''') #''' '''
+print('''4. "문자열"
+연습
+입니다''')# '''   "" '''
+print("""3. 문자열
+연습
+입니다""") #""
+
+print("\n")
+print("\n")
+print("\n")
+
+print(my_string)
+print("\n")
+
+print("Hello ? my name is ","python", "and 20"+"years")
+print("안녕하세요? 제 이름은" + "파이썬이고", 20 ,"세입니다.")
+print("\n")
+
+print("안녕하세요? 제 이름은\n", "\"파이썬\" 입니다.", "c:\\aaa\\bbb\\")
+print("\n")
+
+print(line)
+print("\n")
+print(my_string, "\n")
+print("===============")
+print(message * 3,"\n")
+
+print("상품의 가격은 %s원 입니다" %price) # , 구문자 없음
