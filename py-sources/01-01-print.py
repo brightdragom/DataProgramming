5dc2e9ed9b4cb9e7fab5db74d9a79ff1e513baa2
KimGwangYong <brightdragom@gmail.com>
2020-11-07 11:39:40 +0900

    python_print function

---
+++ py-sources/01-01-print.py
@@ -0,0 +1,10 @@
+#만든 날짜 : 2020.11.03
+#제목 : print() 함수
+#만든이 : 김광용
+#print() 함수 : 출력
+
+my_string = "DSAC Module1 DATA Programming - 선문대학교"
+message = "Hi ! Have great day"
+
+line = " = " * 5
+price = 20000
